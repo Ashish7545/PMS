8196a69470b28f7bc5365374b5482dd85b18a725
Ashish kumar <ashishraj916263@outlook.com>
2023-06-28 11:15:08 +0530

    Update

---
+++ PMSAPI.Models/Authentication/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSAPI.Models.Authentication
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string? Location { get; set; }
+    }
+}
